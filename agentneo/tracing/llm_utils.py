5acf67b9ca2137dd6ffc0cfd7c13839ee33fc892
Kiran Scaria <kiranscaria@outlook.com>
2024-10-17 13:08:01 +0530

    V1.1 (#50)
    * Add crewai example (#42)
    
    add example
    
    * autogen example added (#43)
    
    * bug-fix: error in loading model_costs.json
    
    Signed-off-by: kiranscaria <kiranscaria@outlook.com>
    
    * Digital_Marketing example for crewai (#44)
    
    * added
    
    * Travel Agent Planner (#45)
    
    * added
    
    * remove analysis page(under development)
    
    Signed-off-by: kiranscaria <kiranscaria@outlook.com>
    
    * Delete examples/travel_planner.ipynb
    
    * resolved bugs in json loading
    
    * bump version to v1.1.1
    
    Signed-off-by: kiranscaria <kiranscaria@outlook.com>
    
    ---------
    
    Signed-off-by: kiranscaria <kiranscaria@outlook.com>
    Co-authored-by: fraz akram <77412323+frazakram@users.noreply.github.com>
    Co-authored-by: Vijay Chaurasiya <98342941+vijayc9@users.noreply.github.com>
    Co-authored-by: vijayc9 <vijayc2306@gmail.com>

--- agentneo/tracing/llm_utils.py
+++ agentneo/tracing/llm_utils.py
@@ -13,8 +13,12 @@ try:
     with open("agentneo/configs/model_costs.json", "r") as file:
         config = json.load(file)
 except FileNotFoundError:
-    with resources.open_text("agentneo.configs", "model_costs.json") as file:
+    # with resources.open_text("agentneo", "configs/model_costs.json") as file:
+    #     config = json.load(file)
+    from importlib.resources import files
+    with (files("agentneo.configs") / "model_costs.json").open("r") as file:
         config = json.load(file)
+
 
 
 def extract_llm_output(result):
